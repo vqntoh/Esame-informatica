049e7ed44b6480f2370f7645dbbb7d16272fc65d
vqntoh <130864284+vqntoh@users.noreply.github.com>
2026-06-16 03:59:04 +0200

    Risolto

--- esami2024-25/akinator/main.py
+++ esami2024-25/akinator/main.py
@@ -25,11 +25,129 @@
 # e per i caratteri speciali potete copiare da questi caratteri  [  ]  {  }  <  >
 # print(string.punctuation)
 ## ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
+import random
+
+FILEPATH_DIFFICOLTA = {
+    1 : "facile.txt",
+    2 : "medio.txt",
+    3 : "difficile.txt" 
+    }
+
+LETTERS_LIST = [
+    'a',
+    'b',
+    'c', 
+    'd', 
+    'e', 
+    'f', 
+    'g', 
+    'h', 
+    'i', 
+    'l', 
+    'm', 
+    'n', 
+    'o', 
+    'p', 
+    'q', 
+    'r', 
+    's', 
+    't', 
+    'u', 
+    'v', 
+    'z',
+    'w',
+    'y', 
+    'j',
+    'k', 
+    'x'
+    ]
+
+playing = True
+
+difficulty_path = None
+
+score = 10
+rolled_word = ""
+current_guess = list()
+
+def main():
+    global playing
+    global score
+    
+    games_won = 0
+    games_lost = 0
+
+    while playing:
+        letters_to_try = LETTERS_LIST.copy()
+
+        global score
+        score = 10
+
+        global current_guess
+        current_guess = list()
+
+        global difficulty_path 
+        difficulty_path = roll_diff()
+
+        print(f"Partita numero {games_won + games_lost + 1}")
+        print(f"Il bot seleziona la difficoltà: {difficulty()}")
+        global rolled_word
+        rolled_word = roll_word(difficulty_path)
+        
+        for _ in rolled_word:
+            current_guess.append("_")
+
+        # Partita effettiva
+
+        while score > 0 and len(letters_to_try) > 0 and "".join(current_guess) != rolled_word.strip():
+            guessed = guess(letters_to_try)
+            roll_turn(guessed)
+
+        won = "".join(current_guess) == rolled_word.strip()
+        games_won += int(won)
+        games_lost += int(not won)
+
+        print(f"Complimenti, il bot ha vinto! La parola {rolled_word.upper()} è stata indovinata correttamente" if won else f"Peccato, il bot ha perso! La parola da indovinare era {rolled_word.upper()}")
 
 
-print(open('facile.txt', 'r').read())
-print()
-print(open('medio.txt', 'r').read())
-print()
-print(open('difficile.txt', 'r').read())
-print()
+        playing = True if input("Vuoi continuare a giocare?[S|N] ").lower() == "s" else False
+
+        
+
+    print(f"SESSIONE TERMINATA\nIl bot ha vinto {games_won} partita/e\nIl bot ha perso {games_lost} partita/e")
+
+def roll_diff():
+    return FILEPATH_DIFFICOLTA[random.randint(1, 3)]
+
+def roll_word(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        words = file.readlines()
+        return words[random.randint(0, len(words) - 1)].strip().lower()
+
+def guess(letters_to_try):
+    letter = letters_to_try[random.randint(0, len(letters_to_try) - 1)]
+    letters_to_try.remove(letter)
+    return letter
+
+def roll_turn(guess):
+    
+    global score
+    print(f"Punti {score} - La parola da indovinare è: {formatted_to_guess()}")
+    is_guess_right = rolled_word.count(guess) != 0
+    score -= 1 * int(not is_guess_right)
+    update_current_guess(guess)
+    print(f"\tLettera scelta dal bot: {guess}\n\t{f"Lettera '{guess}' non presente" if not is_guess_right else f"Lettera '{guess}' presente: {formatted_to_guess()}"}")
+
+def update_current_guess(guess):
+    for index, char in enumerate(rolled_word):
+        if char == guess:
+            current_guess[index] = guess
+
+def formatted_to_guess():
+    global current_guess
+    return "".join(current_guess)
+
+def difficulty():
+    return f"{difficulty_path.replace(".txt", "").strip()}"
+
+main()
