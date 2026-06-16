b90973dd8e2abcc3c8faf6ddb6a00d553b90b4c5
vqntoh <130864284+vqntoh@users.noreply.github.com>
2026-06-16 05:21:38 +0200

    Risolto 2024/25 assenti

--- esami2024-25/assenti/main.py
+++ esami2024-25/assenti/main.py
@@ -27,15 +27,40 @@
 ## ! " # $ % & ' ( ) * + , - . / : ; < = > ? @ [ \ ] ^ _ ` { | } ~
 
 
-print(open('prenotati_appello1.txt', 'r').read())
-print()
-print(open('prenotati_appello2.txt', 'r').read())
-print()
-print(open('prenotati_appello3.txt', 'r').read())
-print()
-print(open('consegne_appello1.txt', 'r').read())
-print()
-print(open('consegne_appello2.txt', 'r').read())
-print()
-print(open('consegne_appello3.txt', 'r').read())
-print()
+def leggi_prenotati(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return {parts[0]: parts[1:] for line in file if (parts := line.split())}
+
+def leggi_consegne(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [line.split("_")[2].replace("S", "") for line in file.readlines()]
+    
+def check_assenti(prenotati, consegne):
+    return {prenotazione : prenotati[prenotazione] for prenotazione in prenotati if not prenotazione in consegne}
+
+def print_assenti(assenti, appello):
+    print(f"Assenti al {appello}o appello: ")
+    for matricola, nome in assenti.items():
+        cognome = nome[0]
+        altri_nomi = " ".join(nome[1:])
+        
+        print(f"{matricola:<8} {cognome:<15} {altri_nomi}")
+
+def print_sempre_assenti(assenti):
+    print("Matricole degli studenti che non si sono mai presentati a un appello: ")
+    for matricola in assenti:
+        
+        print(f"{matricola:<8}")
+
+def run():
+    sempre_assenti = list()
+    for i in range(3):
+        appello = i+1
+        assenti = check_assenti(leggi_prenotati(f"prenotati_appello{appello}.txt"), leggi_consegne(f"consegne_appello{appello}.txt"))
+        sempre_assenti = assenti if appello == 1 else [matricola for matricola in assenti if matricola in sempre_assenti]
+        print_assenti(assenti, appello)
+    print_sempre_assenti(sorted(sempre_assenti))
+
+        
+
+run()
